b2ef93538d56f57e58bbaf0bf89ee73d73a5b17e
Hao Guo <jennnnyguo@gmail.com>
2023-05-16 13:44:19 +1000

    commit tree 3fad07467e0f31ea5a6025156caebab87b76cdbf

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Write your code here!
 const main = document.getElementById("main")
-main.remove();
-
-
-const newHeader = document.createElement(`h1`);
-
-newHeader.setAttribute('id', 'victory');
-
-newHeader.textContent = 'is the champion';
-
+main.remove()
+//
+const newHeader = document.createElement("h1")
+//
+newHeader.setAttribute("id", "victory")
+//
+newHeader.textContent = "is the champion"
+// newHeader.textContent = 'is the champion';
